18b7a5f1b7b17fbcb8cf813bb9ad9b39fe747335
baa <34665120+baa-mb@users.noreply.github.com>
2026-07-02 15:15:12 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,53 +1,84 @@
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
-    radio.sendValue("gerade", 0)
-    radio.sendValue("kurve", 0)
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    gang = (gang + 1) % 6
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    basic.showNumber(gang)
 })
-function neigungen () {
-    gerade = Math.min(Math.max(input.rotation(Rotation.Pitch), -45), 45)
-    gerade = Math.round(gerade / g_empfind) * g_empfind
-    kurve = Math.min(Math.max(input.rotation(Rotation.Roll), -45), 45)
+function berechne_rad_werte () {
+    kurve_links = kurve_rad * -1
+    kurve_rechts = kurve_rad
+    gerade_links = gerade_rad * rad_links_korrektur
+    gerade_rechts = gerade_rad
+    links_soll = Math.round((gerade_links + kurve_links * feinheit) / 2)
+    rechts_soll = Math.round((gerade_rechts + kurve_rechts * feinheit) / 2)
 }
-input.onButtonPressed(Button.B, function () {
-    radio.sendValue("get_dist", 1)
-})
-radio.onReceivedValue(function (name, value) {
-    if (name == "distanz") {
-        basic.showNumber(value)
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
+// robotbit.Servo(robotbit.Servos.S1, 0)
+function init () {
+    basic.showIcon(IconNames.Diamond)
+    hebe_winkel = 70
+    motor_rechts = robotbit.Motors.M1A
+motor_links = robotbit.Motors.M2A
+feinheit = 0.4
+    robotbit.MotorStopAll()
+}
+radio.onReceivedValue(function (info, wert) {
+    grenze = 205 + gang * 10
+    // serial.writeValue("kurve_rad", kurve_rad)
+    if (info == "gerade") {
+        gerade_get = wert
+        gerade_rad = Math.round(Math.map(gerade_get, -45, 45, -1 * grenze, grenze))
+    } else if (info == "kurve") {
+        kurve_get = wert
+        kurve_rad = Math.round(Math.map(kurve_get, -45, 45, -255, 255))
+    } else if (info == "get_dist") {
+        radio.sendValue("distanz", sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+        ))
     }
 })
-let kurve = 0
-let gerade = 0
-let g_empfind = 0
+let kurve_get = 0
+let gerade_get = 0
+let grenze = 0
+let hebe_winkel = 0
+let rechts_soll = 0
+let links_soll = 0
+let gerade_rechts = 0
+let gerade_rad = 0
+let gerade_links = 0
+let kurve_rechts = 0
+let kurve_rad = 0
+let kurve_links = 0
+let gang = 0
+let feinheit = 0
+let rad_links_korrektur = 0
 radio.setGroup(99)
-basic.showString("99")
-g_empfind = 5
-let ist_oben = true
-let alt_gerade = -99
-let alt_kurve = -99
-let k_empfind = 1
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    `)
+rad_links_korrektur = 1
+let rad_links = 0
+let rad_rechts = 0
+let motor_links = 0
+let motor_rechts = 0
+let links_ist = 0
+let rechts_ist = 0
+feinheit = 0.5
+init()
 basic.forever(function () {
-    neigungen()
-    if (input.buttonIsPressed(Button.A)) {
-        if (gerade != alt_gerade || kurve != alt_kurve) {
-            radio.sendValue("gerade", gerade)
-            radio.sendValue("kurve", kurve)
-        }
+    berechne_rad_werte()
+    if (links_ist < links_soll) {
+        links_ist = Math.min(links_ist + 12, links_soll)
+    } else if (links_ist > links_soll) {
+        links_ist = Math.max(links_ist - 12, links_soll)
     }
-    alt_gerade = gerade
-    alt_kurve = kurve
+    if (rechts_ist < rechts_soll) {
+        rechts_ist = Math.min(rechts_ist + 12, rechts_soll)
+    } else if (rechts_ist > rechts_soll) {
+        rechts_ist = Math.max(rechts_ist - 12, rechts_soll)
+    }
+    if (links_soll == 0 && rechts_soll == 0 && links_ist == 0 && rechts_ist == 0) {
+        robotbit.MotorStopAll()
+    } else {
+        robotbit.MotorRun(motor_links, links_ist)
+robotbit.MotorRun(motor_rechts, rechts_ist)
+    }
+    basic.pause(10)
 })
